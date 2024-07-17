9ef3aaaa24abce8e5dda6a7f8ddfb1246deae583
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-07-17 20:49:13 +0500

    Get started screen design is almost 65-70 percent completed

--- lib/pages/Get_Started.dart
+++ lib/pages/Get_Started.dart
@@ -10,9 +10,116 @@ class GetStartedScreen extends StatefulWidget {
 class _GetStartedScreenState extends State<GetStartedScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            const Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'AI',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontFamily: 'Calistoga',
+                      fontWeight: FontWeight.normal,
+                      color: Color(0xffC366BA)),
+                ),
+                Text(
+                  'Mate',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontFamily: 'Calistoga',
+                      fontWeight: FontWeight.normal,
+                      color: Color(0xff2A8FBA)),
+                )
+              ],
+            ),
+            const Row(
+              children: [
+                SizedBox(width: 10),
+                Text(
+                  'Your Personal Assistant',
+                  style: TextStyle(
+                      height: 1,
+                      fontFamily: 'Halant',
+                      fontSize: 26,
+                      fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Opacity(
+                  opacity: 0.5,
+                  child: Text(
+                    'Enhance your productivity health',
+                    style: TextStyle(
+                        height: 1,
+                        fontFamily: 'Halant',
+                        fontSize: 22,
+                        fontWeight: FontWeight.w300),
+                  ),
+                )
+              ],
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Opacity(
+                  opacity: 0.5,
+                  child: Text(
+                    'and daily life with advanced AI-',
+                    style: TextStyle(
+                        height: 1,
+                        fontFamily: 'Halant',
+                        fontSize: 22,
+                        fontWeight: FontWeight.w300),
+                  ),
+                )
+              ],
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Opacity(
+                  opacity: 0.5,
+                  child: Text(
+                    'Powered features',
+                    style: TextStyle(
+                        height: 1,
+                        fontFamily: 'Halant',
+                        fontSize: 22,
+                        fontWeight: FontWeight.w300),
+                  ),
+                )
+              ],
+            ),
+            Image.asset(
+              'images/main_gif.gif',
+              height: 500,
+              width: 300,
+            ),
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('Buttons for login and signup are placed here')],
+            ),
+          ],
+        ),
       ),
     );
   }
