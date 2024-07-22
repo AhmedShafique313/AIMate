50386812ed625bd5a9870dd19e7ed7e726573fec
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-07-22 18:13:46 +0500

    3 rows developed in the signup page

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -10,6 +10,86 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  'Create your ',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'GentiumPlus'),
+                ),
+                Text(
+                  'AI',
+                  style: TextStyle(
+                      fontFamily: 'GentiumPlus',
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xffC366BA)),
+                ),
+                Text(
+                  'Mate ',
+                  style: TextStyle(
+                      fontFamily: 'GentiumPlus',
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff2A8FBA)),
+                ),
+                Text(
+                  'Account',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'GentiumPlus'),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 8,
+                ),
+                Opacity(
+                  opacity: 0.5,
+                  child: Text(
+                    'Join AIMate and start your journey to enhance',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: 'Halant'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 8,
+                ),
+                Opacity(
+                    opacity: 0.5,
+                    child: Text(
+                      'productivity and well-being',
+                      style: TextStyle(
+                          height: 1,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300,
+                          fontFamily: 'Halant'),
+                    )),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
